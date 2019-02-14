233d5ba56d07be0f420d9fb3d58e569946e7facf
floge77 <floge77@gmail.com>
2019-02-14 22:27:56 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -55,7 +55,10 @@ module.exports = NodeHelper.create({
 
         var self = this;
 
-        var mqttServer = (server.address.match(/^mqtts?:\/\//) ? '' : 'mqtt://') + server.address + ':' server.port+ ;
+        var mqttServer = (server.address.match(/^mqtts?:\/\//) ? '' : 'mqtt://') + server.address;
+        if (server.port) {
+            mqttServer = mqttServer + ':' + server.port
+        }
         console.log(self.name + ': Connecting to ' + mqttServer);
 
         server.client = mqtt.connect(mqttServer, server.options);
