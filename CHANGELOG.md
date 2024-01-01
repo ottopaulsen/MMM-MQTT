a6248966d320aa2f8e4a01704bd8df1150d4a17a
Otto Paulsen <otto@OttoTdMac.local>
2024-01-01 21:35:11 +0100

    Save all values in node_helper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 Changes worth noticing, per version, starting from version 1.1.1
 
+### 1.2.0
+
+- Save values in node_helper so clients get them also if they start later.
+  NB! If you are using `broadcast`, be aware that all values are broadcasted when any value is updated.
+- Update dependencies
+
 ### 1.1.1
 
 - Fix bug making multiple connections
--- MMM-MQTT.js
+++ MMM-MQTT.js
@@ -73,15 +73,17 @@ Module.register("MMM-MQTT", {
   setSubscriptionValue: function (subscriptions, payload, useWildcards) {
     for (let i = 0; i < subscriptions.length; i++) {
       sub = subscriptions[i];
-      if (
-        sub.serverKey == payload.serverKey && useWildcards
-          ? topicsMatch(sub.topic, payload.topic)
-          : sub.topic == payload.topic
+      const savedValues = new Map(Object.entries(JSON.parse(payload)))
+      const savedValue = savedValues.get(sub.serverKey + "-" + sub.topic)
+      if (savedValue &&
+        (sub.serverKey == savedValue.serverKey && useWildcards
+          ? topicsMatch(sub.topic, savedValue.topic)
+          : sub.topic == savedValue.topic)
       ) {
-        var value = payload.value;
+        var value = savedValue.value;
 
         if (sub.broadcast) {
-          this.sendNotification("MQTT_MESSAGE_RECEIVED", payload);
+          this.sendNotification("MQTT_MESSAGE_RECEIVED", savedValue);
         }
 
         // Extract value if JSON Pointer is configured
@@ -104,7 +106,7 @@ Module.register("MMM-MQTT", {
           }
         }
         sub.value = value;
-        sub.time = payload.time;
+        sub.time = savedValue.time;
       }
     }
     return subscriptions;
